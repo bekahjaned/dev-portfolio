75cf57d771723f7127173ea321f1c9f4c09c580c
Rebekah <rebekahjanedejong@gmail.com>
2021-08-23 18:15:38 -0400

    Create NavBar component

---
+++ src/Components/NavBar/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { NavBarWrap } from '../../Elements/NavBarWrap/'
+
+function NavBar() {
+    return (
+        <NavBarWrap>
+            <div className="nav-links">
+                <a href="#about">About</a>
+                <a href="#work">Work</a>
+                <a href="#skills">Skills</a>
+                <a href="#contact">Contact</a>
+            </div>
+        </NavBarWrap>
+    )
+}
+
+export default NavBar
---
+++ src/Elements/NavBarWrap/index.js
@@ -0,0 +1,53 @@
+import styled from 'styled-components'
+
+const media = { 
+    desktop: "@media(min-width: 1080px)",
+    tablet: "@media(min-width: 768px)"
+ };
+
+export const NavBarWrap = styled.nav`
+    width: 100vw;
+    height: 50px;
+    background-color: #0B3B40;
+    display: flex;
+    align-items: center;
+    justify-content: right;
+
+    .nav-links {
+        padding-right: 35px; 
+    }
+
+    a {
+        color: #FBFEF9;
+        text-decoration: none;
+        padding-left: 18px;
+        font-size: 0.75rem;
+    }
+
+    a:hover {
+        color: #FF68A2;
+    }
+
+    ${media.tablet} {
+        .nav-links {
+            padding-right: 84px;  
+        } 
+
+        a {
+            font-size: 0.875rem;
+            padding-left: 20px;
+        }
+    }
+
+    ${media.desktop} {
+        .nav-links {
+            padding-right: 180px;  
+        }
+
+        a {
+            font-size: 1rem;
+            padding-left: 22px;
+        }
+    
+}
+`
