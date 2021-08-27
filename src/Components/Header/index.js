c0a758cdafeb35e79694afef3d542db59f58d256
Rebekah <rebekahjanedejong@gmail.com>
2021-08-27 09:57:06 -0400

    Add non-breaking space in De Jong

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -8,7 +8,7 @@ function Header({ theme }) {
     return (
         <HeaderWrap backgound={theme.backgound} color={theme.color}>
             <div className="header-text">
-                <h1>Rebekah De Jong</h1>
+                <h1>Rebekah De&nbsp;Jong</h1>
                 <p>A front-end developer who is passionate about doing good & helping others do good</p>
             </div>
             <img src={MyPhoto} alt="me" />
